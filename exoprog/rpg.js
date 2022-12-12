26e1348ed03ed95343ecc81019c6f020afd54b09
FerdidBe <118370384+FerdidBe@users.noreply.github.com>
2022-12-12 11:46:04 +0100

    Add files via upload

---
+++ exoprog/rpg.js
@@ -0,0 +1,15 @@
+mechants = document.getElementsByClassName("mechant")
+
+blockattaque = document.getElementById("textattaque")
+pvjoueur1=document.getElementById("pvjoueur1")
+pvjoueur2=document.getElementById("pvjoueur2")
+pvjoeur3=document.getElementById("pvjoueur3")
+pvjoueur4=document.getElementById("pvjoueur4")
+
+pvmechant1=
+
+blockattaque.onclick = function() {
+    console.log("perdpv")
+}
+
+blockdefence= document.getElementById("textdefence")
